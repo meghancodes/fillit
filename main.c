b87a101d79399d0665ae72ff82708e3ba30d6286
Meghan Williams <mwilliam@e1z2r4p9.42.us.org>
2017-02-01 20:05:26 -0800

    Slightly updated main

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-int		main(int c, char **args)
+// int		main(int c, char **args)
+// {
+// 	if (c == 2)
+// 		args[1] = "c";
+// 	return (0);
+// }
+
+int main (int argc, char *argv[])
 {
-	if (c == 2)
-		args[1] = "c";
-	return (0);
+	
+	if (argc !=  )
+	{
+		//error here
+	}
+
+	fd = open();
+
+	if (fd == -1)
+	{
+		//error here
+	}
+
+	
 }
