217a8e83d7bc47330ce3e8265578e51c1be98edc
Ming Horn <minghaihorn@gmail.com>
2017-02-03 15:57:05 -0800

    Rewrote fillit types to be more readable

--- fillit.h
+++ fillit.h
@@ -24,17 +24,25 @@ typedef struct	s_map
 
 typedef struct	s_types
 {
-	int		O1[4][2];
-	int		T1[4][2];
-	int		I1[4][2];
-	int		L1[4][2];
-	int		J1[4][2];
-	int		Z1[4][2];
-	int		S1[4][2];
+	int		**O1;
+	int		**T1;
+	int		**I1;
+	int		**L1;
+	int		**J1;
+	int		**Z1;
+	int		**S1;
 }				t_types;
 
-static const t_types types = {{{0,0}, {0,1}, {1,0}, {1,1}}, {{0,0}, {0,1}, {0,2}, {1,1}}, {{0,0}, {1,0}, {2,0}, {3,0}}, {{0,0}, {1,0}, {2,0}, {2,1}}, {{0,1}, {1,1}, {2,0}, {2,1}}, {{0,0}, {0,1}, {1,1}, {1,2}}, {{0,1}, {0,2}, {1,0},{1,1}};
+int				o1[4][2] = {{0,0}, {0,1}, {1,0}, {1,1}};
+int				t1[4][2] = {{0,0}, {0,1}, {0,2}, {1,1}};
+int				i1[4][2] = {{0,0}, {1,0}, {2,0}, {3,0}};
+int				l1[4][2] = {{0,0}, {1,0}, {2,0}, {2,1}};
+int				j1[4][2] = {{0,1}, {1,1}, {2,0}, {2,1}};
+int				z1[4][2] = {{0,0}, {0,1}, {1,1}, {1,2}};
+int				s1[4][2] = {{0,1}, {0,2}, {1,0}, {1,1}};
 
+
+static const t_types types = {(int **)o1, (int **)t1, (int **)i1, (int **)l1, (int **)j1, (int **)z1, (int **)s1};
 t_tet			*new_tet(char *type, char order, int x, int y);
 void			set_tet(t_tet *node, t_map *map, int x, int y);
 int				read_in(int fd);
@@ -45,5 +53,5 @@ char			*ij_tet_types(char *type_string);
 char			*l_tet_types(char *type_string);
 char			*zs_tet_types(char *type_string);
 char			*tet_string(char **buf, char *type_string);
-char			*find_tet_type(char *type_string)
+char			*find_tet_type(char *type_string);
 #endif
