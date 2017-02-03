ff5d5c4b3a051b03090ab8793d25467625dc77d1
Meghan Williams <mwilliam@e1z3r9p9.42.us.org>
2017-02-03 11:25:18 -0800

    Expression changes

--- tet_types.c
+++ tet_types.c
@@ -14,13 +14,13 @@ char *ot_tet_types(char *type_string)
 
 	if (ft_strcmp(type_string, o) == 0)
 		return ("o");
-	if (ft_strcmp(type_string, t) == 0)
+	else if (ft_strcmp(type_string, t) == 0)
 		return ("t1");
-	if (ft_strcmp(type_string, t2) == 0)
+	else if (ft_strcmp(type_string, t2) == 0)
 		return ("t2");
-	if (ft_strcmp(type_string, t3) == 0)
+	else if (ft_strcmp(type_string, t3) == 0)
 		return ("t3");
-	if (ft_strcmp(type_string, t4) == 0)
+	else if (ft_strcmp(type_string, t4) == 0)
 		return ("t4");
 	else
 		return (NULL);
@@ -37,15 +37,15 @@ char *ij_tet_types(char *type_string)
 
 	if (ft_strcmp(type_string, i) == 0)
 		return ("i");
-	if (ft_strcmp(type_string, i2) == 0)
+	else if (ft_strcmp(type_string, i2) == 0)
 		return ("i2");
-	if (ft_strcmp(type_string, j) == 0)
+	else if (ft_strcmp(type_string, j) == 0)
 		return ("j");
-	if (ft_strcmp(type_string, j2) == 0)
+	else if (ft_strcmp(type_string, j2) == 0)
 		return ("j2");
-	if (ft_strcmp(type_string, j3) == 0)
+	else if (ft_strcmp(type_string, j3) == 0)
 		return ("j3");
-	if (ft_strcmp(type_string, j4) == 0)
+	else if (ft_strcmp(type_string, j4) == 0)
 		return ("j4");
 	else
 		return (NULL);
@@ -60,11 +60,11 @@ char *l_tet_types(char *type_string)
 
 	if (ft_strcmp(type_string, l3) == 0)
 		return ("l");
-	if (ft_strcmp(type_string, l4) == 0)
+	else if (ft_strcmp(type_string, l4) == 0)
 		return ("l2");
-	if (ft_strcmp(type_string, l5) == 0)
+	else if (ft_strcmp(type_string, l5) == 0)
 		return ("l3");
-	if (ft_strcmp(type_string, l6) == 0)
+	else if (ft_strcmp(type_string, l6) == 0)
 		return ("l4");
 	else
 		return (NULL);
@@ -79,11 +79,11 @@ char *z_tet_types(char *type_string)
 
 	if (ft_strcmp(type_string, z) == 0)
 		return ("z1");
-	if (ft_strcmp(type_string, z2) == 0)
+	else if (ft_strcmp(type_string, z2) == 0)
 		return ("z2");
-	if (ft_strcmp(type_string, z3) == 0)
+	else if (ft_strcmp(type_string, z3) == 0)
 		return ("z3");
-	if (ft_strcmp(type_string, z4) == 0)
+	else if (ft_strcmp(type_string, z4) == 0)
 		return ("z4");
 	else
 		return (NULL);
