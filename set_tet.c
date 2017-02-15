8b6f498c993ef46124a7e895f359ecf9de235e01
Meghan Williams <mwilliam@e1z2r9p34.42.us.org>
2017-02-15 12:22:10 -0800

    Slight update

--- set_tet.c
+++ set_tet.c
@@ -3,7 +3,9 @@
 int		valid_set(t_tet *node, t_map *map)
 {
 	//a function to recognize the tet type
-
+	node->x = x;
+	node->y = y;
+	
 	if (map->arr[x][y] == 1)
 		return (0);
 	else if (map->arr[x][y] == 0 && map->arr[x + 1][y + 1] == 1 || )
