8cfc1655938a579e39a822a394c18f7ad7a0f070
Meghan Williams <mwilliam@e1z2r3p1.42.us.org>
2017-04-03 15:30:15 -0700

    Altered solver

--- solver.c
+++ solver.c
@@ -25,6 +25,7 @@ int solve(t_lst *tets, t_map *map)
 	int y;
 	
 	x = 0;
+	tets->current = tets->head;
 	while (tets->current->next != NULL && x < map->size)
 	{
 		y = 0;
@@ -34,8 +35,12 @@ int solve(t_lst *tets, t_map *map)
 			{
 				set_tet(tets->current, map, x, y);
 				tets->current = tets->current->next;
-				if (solve(tets, map))
+				print_map(map);
+				if (tets->current == NULL)
+				{
+					ft_putstr("does this work!!!?\n");
 					return (1);
+				}
 			}
 			y++;
 		}
